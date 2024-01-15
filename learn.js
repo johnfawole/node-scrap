a39164ff1051c843ba734dd437340dd068efd3f0
John Fáwọlé <johnfawole18@gmail.com>
2024-01-15 20:41:25 +0100

    add scraping

--- learn.js
+++ learn.js
@@ -1,12 +1,39 @@
+// defining fs & puppeteer
+const fs = require ('fs');
 const puppeteer = require ('puppeteer');
 
  async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.blockchainalpha.tech');
+    await page.goto('https://www.traversymedia.com/guide');
 
-    await page.screenshot({ path: 'example.jpg', fullPage: true});
+    // await page.screenshot({ path: 'example.jpg', fullPage: true});
+    // await browser.close();
+
+    const courses = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('#courses .card'), (e) => ({
+        title: e.querySelector('.card-body h3') .innerText,
+        level: e.querySelector('.card-body .level') .innerText,
+        url: e.querySelector('.card-footer a') .href,
+        promo: e.querySelector('.card-footer .promo-code .promo') .innerText,
+    }))
+    );
+
+    console.log(courses);
+
+    //save to JSON
+    fs.writeFile('brad.json', JSON.stringify(courses), (err) => {
+        if (err) throw err;
+        console.log('Saved the File Now, buddy')
+    }
+    );
+     
     await browser.close();
  }
 
-  run();
+ run();
+  
+
+
+  
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Acknowledgement
+
+I learned how to scrap with Traversy's material on [YouTube](https://www.youtube.com/watch?v=S67gyqnYHmI&t=714s.)
+
+## What I Learned
+
+* I learned how to screenshot and save websites to PDF.
+* I learned how to scrap with Node.js. I mean, because I am more comfortable with Node, then using Python for this.
